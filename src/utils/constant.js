96e92a83509234b76c1bf3c2f4eeb81ddf6d2661
Aldy Revigustian <aldybudiasih@gmail.com>
2021-11-13 23:37:45 +0700

    Fixed Cors

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,11 +1,11 @@
-export const API_URL_ONLIVES = "https://www.showroom-live.com/api/live/onlives"
-export const API_URL_STREAM = "https://www.showroom-live.com/api/live/streaming_url?room_id="
-export const API_URL_PROFILE = "https://www.showroom-live.com/api/room/profile?room_id="
-export const API_URL_COMMENT = "https://www.showroom-live.com/api/live/comment_log?room_id="
-export const API_URL_RANK = "https://www.showroom-live.com/api/live/stage_user_list?room_id="
-export const API_URL_USER = "https://www.showroom-live.com/api/live/current_user?room_id=373546"
+export const API_URL_ONLIVES = "/live/onlives"
+export const API_URL_STREAM = "/live/streaming_url?room_id="
+export const API_URL_PROFILE = "/room/profile?room_id="
+export const API_URL_COMMENT = "/live/comment_log?room_id="
+export const API_URL_RANK = "/live/stage_user_list?room_id="
+export const API_URL_USER = "/live/current_user?room_id=373546"
 export const API_URL_SR = "https://www.showroom-live.com/"
-export const API_URL_GIFT = "https://www.showroom-live.com/api/live/gift_log?room_id="
+export const API_URL_GIFT = "/live/gift_log?room_id="
 
 export const memberId = [
     "318207",
@@ -35,4 +35,5 @@ export const memberId = [
     "317724",
     "318208",
     "318229",
+    "356039",//
 ];
